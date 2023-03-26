4751dac98e0d8d142cba8e41dfb845a0f353a047
Yogesh Psndey <103723595+yogeshpandey143@users.noreply.github.com>
2023-03-26 12:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// get all panel elements
+const panels = document.querySelectorAll('.panel');
+
+// add click event listener to each panel
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+// remove active class from all panels
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
+
+// set the first panel as active by default
+panels[0].classList.add('active');
